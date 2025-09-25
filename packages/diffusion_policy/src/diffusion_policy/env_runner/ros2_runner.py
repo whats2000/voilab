b89cfce01fdd7e840624984f2cab8304f061cefa
author31 <wi072815.tw@gmail.com>
2025-09-25 23:25:41 +0800

    feat: add ros2 skeleton

---
+++ packages/diffusion_policy/src/diffusion_policy/env_runner/ros2_runner.py
@@ -0,0 +1,10 @@
+from typing import Dict
+from diffusion_policy.policy.base_image_policy import BaseImagePolicy
+from diffusion_policy.env_runner.base_image_runner import BaseImageRunner
+
+class ROS2Runner(BaseImageRunner):
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
+    def run(self, policy: BaseImagePolicy) -> Dict:
+        raise NotImplementedError()
---
+++ packages/diffusion_policy/src/diffusion_policy/environments/ros2/ros2_env.py
@@ -0,0 +1,15 @@
+class ROS2Env:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def get_obs(self):
+        pass
+
+    def step(self):
+        pass
+
+    def close(self):
+        pass
